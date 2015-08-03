3a5fa8c614283ddf59dd2e626a63863d0e0fbc08
Hao Men <hao.men@gmail.com>
2015-08-03 01:00:22 -0400

    update to get all news from important news section

--- financialNews/crawChineseFinance.py
+++ financialNews/crawChineseFinance.py
@@ -23,6 +23,7 @@ class SinaFinancialNews:
         return sinaHTML
 
     #find section header url, name uid with given section name
+    #fine from m-list-container
     def getNewsFromSection(self,secName,sinaHTML):
         section=sinaHTML.split(secName)[-1].split('</ul>')[0].split('<li>')[1:-1]
         for item in section:
@@ -37,23 +38,51 @@ class SinaFinancialNews:
             if self.collection.find_one({"_id":uuid})==None:
                 self.secCon.append({"_id":uuid,"date":date,"uid":uid,"date":date,"name":name,"url":url,"source":"Sina Finance"})
     
+    #fine h3 news from blk_yw_1
+    def getHeaderNewsFromNews(self,sinaHTML):
+        section=sinaHTML.split('blk_yw_1')[-1].split('</div>')[0].split('href="')[1:]
+
+        for item in section:
+            url=item.split('"')[0].strip()
+            if url.split('.')[-1]=='shtml':
+                date=url.split('/')[-2]
+                uid=url.split('/')[-1].rstrip('.shtml')
+                uuid=date+'-'+uid
+                name=item.split('>')[1].rstrip('</a>')
+                name=name.decode('gbk').encode('utf-8')
+                
+                #confirm item not exist in mongo db
+                if self.collection.find_one({"_id":uuid})==None:
+                    self.secCon.append({"_id":uuid,"date":date,"uid":uid,"date":date,"name":name,"url":url,"source":"Sina Finance"})
+
+        
+    
+
     #get all items 
     def getAllSections(self):
         page=self.getHTMLNewsPage()
+
+        #get news from blk_yw_1
+        self.getHeaderNewsFromNews(page)
+
         for sec in ['blk_yw_2','blk_yw_3','blk_yw_4']:
             self.getNewsFromSection(sec,page)
 
     #print out extracted secCon which inserts into mongodb
     def printSection(self):
+        print len(self.secCon)
         for item in self.secCon:
             print item["_id"]
             print item
             print ''
+        print self.secCon
 
     def insertIntoMongo(self):
-        self.collection.insert_many(self.secCon)
+        if(len(self.secCon)>0):
+            self.collection.insert_many(self.secCon)
 
-#print sec1Con
+
+#get sina finance news and insert into mongo
 sina=SinaFinancialNews()
 sina.getAllSections()
 #sina.printSection()
